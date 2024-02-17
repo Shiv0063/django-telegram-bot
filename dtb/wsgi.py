8384fd9ff01d37e200f2faab5519a3e0acf8377c
Vishal Barot <97306828+Shiv0063@users.noreply.github.com>
2024-02-17 14:02:01 +0530

    Update wsgi.py

--- dtb/wsgi.py
+++ dtb/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for dtb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+from run_polling import run_polling
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
 
 application = get_wsgi_application()
+
+
+if __name__ == "__main__":
+    run_polling()
